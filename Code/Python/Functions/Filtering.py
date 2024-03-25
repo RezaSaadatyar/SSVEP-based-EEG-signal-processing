04cf90f542d7bc504867138138f7637a43a571ae
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-03-25 21:19:37 +0330

    filtering

--- Code/Python/Functions/Filtering.py
+++ Code/Python/Functions/Filtering.py
@@ -2,17 +2,22 @@ import numpy as np
 from scipy import signal
 
 # ================================================ Filtering =============================================================  
-def filtering(data, f_low, f_high, order, fs, type_filter): # Function to apply digital filtering to data
+# Function to apply digital filtering to data
+def filtering(data, f_low, f_high, order, fs, f_notch, notch_filter="on", type_filter='bandpass'): 
     
     if type(data).__name__ != 'ndarray': # Convert data to ndarray if it's not already
         data = np.array(data)
     if data.ndim > 1 and data.shape[0] > data.shape[1]: # Transpose data if it has more columns than rows
         data = data.T
         
-    f_low = f_low / (fs / 2) # Normalize frequency values
+    f_low = f_low / (fs / 2)     # Normalize frequency values
     f_high = f_high / (fs / 2)
     
-    if type_filter == "low": # Design Butterworth filter based on the specified type
+    if notch_filter == "on":     # Notch filter
+        b, a = signal.butter(order, [(f_notch-0.6)/fs/2, (f_notch+0.6)/fs/2], btype='bandstop')
+        data = signal.filtfilt(b, a, data)
+        
+    if type_filter == "low":     # Design Butterworth filter based on the specified type
         b, a = signal.butter(order, f_low, btype='low')
     elif type_filter == "high":
         b, a = signal.butter(order, f_high, btype='high')
@@ -20,7 +25,7 @@ def filtering(data, f_low, f_high, order, fs, type_filter): # Function to apply 
         b, a = signal.butter(order, [f_low, f_high], btype='bandpass')
     elif type_filter == "bandstop":
         b, a = signal.butter(order, [f_low, f_high], btype='bandstop')
-        
-    data_filter = signal.filtfilt(b, a, data) # Apply the digital filter using filtfilt to avoid phase distortion
-    
+
+    # Apply the digital filter using filtfilt to avoid phase distortion
+    data_filter = signal.filtfilt(b, a, data)
     return data_filter  
