0becf3f8b4b89c2c0cdd4ccc835f1e486c7fa30d
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-03-26 19:26:46 +0330

    filtering

--- Code/Python/Functions/Filtering.py
+++ Code/Python/Functions/Filtering.py
@@ -3,7 +3,7 @@ from scipy import signal
 
 # ================================================ Filtering =============================================================  
 # Function to apply digital filtering to data
-def filtering(data, f_low, f_high, order, fs, f_notch, notch_filter="on", type_filter='bandpass'): 
+def filtering(data, f_low, f_high, order, fs, notch_freq, quality_factor, filter="on", notch_filter="on", type_filter='bandpass'): 
     # ------------------------------------ Normalize frequency values ------------------------------------
     f_low = f_low / (fs / 2)     
     f_high = f_high / (fs / 2)
@@ -24,21 +24,23 @@ def filtering(data, f_low, f_high, order, fs, f_notch, notch_filter="on", type_f
         b, a = signal.butter(order, [f_low, f_high], btype='bandpass')
     elif type_filter == "bandstop":
         b, a = signal.butter(order, [f_low, f_high], btype='bandstop')
+    
+    # Design a notch filter using signal.iirnotch
+    b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, fs)
     # ------------------------------------------- Notch filter -------------------------------------------
-    if data.ndim == 3:
-        for i in range(data.shape[0]):
-            if notch_filter == "on":     
-                b1, a1 = signal.butter(order, [(f_notch - 0.7) / fs / 2, (f_notch + 0.7) / fs / 2], btype='bandstop')
-                data[i, :, :] = signal.filtfilt(b1, a1, data[i, :, :])
-            # ----------- Apply the digital filter using filtfilt to avoid phase distortion --------------
-            # data[i, :, :] = signal.filtfilt(b, a, data[i, :, :])
-    elif data.ndim < 3:
-        # ----------------------------------------- Notch filter -----------------------------------------
-        if notch_filter == "on":     
-            b1, a1 = signal.butter(order, [(f_notch - 0.6) / fs / 2, (f_notch + 0.6) / fs / 2], btype='bandstop')
-            data = signal.filtfilt(b1, a1, data)
-        # ------------- Apply the digital filter using filtfilt to avoid phase distortion ----------------    
-        # data = signal.filtfilt(b, a, data) 
+    if notch_filter == "on":
+        if data.ndim == 3:
+            for i in range(data.shape[0]):
+                data[i, :, :] = signal.filtfilt(b_notch, a_notch, data[i, :, :])
+        elif data.ndim < 3:
+            data = signal.filtfilt(b_notch, a_notch, data)
+     # -------------- Apply the digital filter using filtfilt to avoid phase distortion ------------------
+    if filter == "on":
+        if data.ndim == 3:
+            for i in range(data.shape[0]):
+                data[i, :, :] = signal.filtfilt(b, a, data[i, :, :])
+        elif data.ndim < 3:
+            data = signal.filtfilt(b, a, data)
     # ------------------------ Transpose data if it has more columns than rows ---------------------------
     if data.ndim == 2 and data.shape[0] < data.shape[1]: 
         data = data.T
