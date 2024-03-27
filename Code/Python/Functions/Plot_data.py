c451f37c937dbd98d74b05a34029ee982da3994a
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-03-28 01:43:38 +0330

    plot data

--- Code/Python/Functions/Plot_data.py
+++ Code/Python/Functions/Plot_data.py
@@ -22,23 +22,22 @@ def plot_data(data, fs=None, channels_name=None, first_point=0, last_point=100, 
         
         if type(data).__name__ != 'ndarray': # Convert data to ndarray if it's not already
             data = np.array(data)
-            
-        if data.ndim < 3 and data.shape[0] < data.shape[1]: 
-            data = data.T
-
-        if channels_name is None: # If channel names are not provided, use numerical labels
-            channels_name = np.arange(1, data.shape[1]+1)  
+        
+        # Transpose the data if it has more than one dimension and has fewer rows than columns
+        data = data.T if data.ndim > 1 and data.shape[0] < data.shape[-1] else data
+        
+        # Use numerical labels if channel names are not provided
+        channels_name = channels_name if channels_name is not None else np.arange(1, data.shape[1] + 1)
 
         if first_point > len(data) or last_point > len(data):
             print(f"Error, {first_point = } or {last_point = } > data size: {len(data)}")
             return
-            
+      
         data = data[first_point:last_point,:] # Extract the specified range of data
-        std = np.sort(np.std(data, axis=0)) # Calculate the standard deviation of each channel
-        if len(std) > 100:
-            std = np.mean(std[1:len(std)-1])
-        else:
-            std = np.mean(std)
+        std = np.sort(np.std(data, axis=0))   # Calculate the standard deviation of each channel
+        
+        # If the array is larger than 100 elements, exclude the first and last elements before taking the mean
+        std = np.mean(std[1:-1] if len(std) > 100 else std)
         
         _, axs = plt.subplots(nrows=1, sharey='row', figsize=size_fig) # Create subplots for the figure
         offset = np.arange(len(channels_name)*std*val_ylim, 0, -std*val_ylim)
@@ -51,22 +50,27 @@ def plot_data(data, fs=None, channels_name=None, first_point=0, last_point=100, 
             line = axs.plot(data + offset, linewidth=1)
             axs.set_xlabel('sample', fontsize=10)
         
-        axs.set_title(f"Channels: {len(channels_name)}#; {title}", fontsize=10) # Set subplot properties
+        axs.grid(False)
         axs.set_yticks(offset)
-        axs.set_yticklabels(channels_name,  weight='bold')
         axs.tick_params(axis='x', labelsize=9)
         axs.tick_params(axis='y', labelsize=8)
         axs.set_ylabel('Channels', fontsize=10)
+        axs.set_yticklabels(channels_name,  weight='bold')
         axs.tick_params(axis='y', color='k', labelcolor='k')
-        axs.grid(False)
+        axs.set_title(f"Channels: {len(channels_name)}#; {title}", fontsize=10) # Set subplot properties
         
         ytick_labels = plt.gca().get_yticklabels() # Color lines and labels
         for i, label in enumerate(ytick_labels):
             # line[i].set_color(line[i].get_color())
-            label.set_color(line[i].get_color())
+            label.set_color(line[i].get_color())   # Set label color to match the line color
         
         # Auto-scale x-axis and set y-axis limits
         axs.autoscale(enable=True, axis="x",tight=True)
         min_ = np.min(np.min(data + offset, axis=0))
         max_ = np.max(np.max(data + offset, axis=0))
         axs.set_ylim(min_ + min_*0.02, max_ + max_*0.01)
+        
+             # if len(std) > 100:
+        #     std = np.mean(std[1:len(std)-1])
+        # else:
+        #     std = np.mean(std)
