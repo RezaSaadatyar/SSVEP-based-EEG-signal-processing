61ae6b371168db9efbd6d27c80a24e745c4d5638
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-03-29 02:51:20 +0330

    function

--- Code/MATLAB/Functions/car_filter.m
+++ Code/MATLAB/Functions/car_filter.m
@@ -1,7 +1,15 @@
-function x_car=car_filter(input)
-mean_= mean(input,2);                   
-for j=1:size(input,2)
-    input(:,j)=input(:,j)-mean_;
+function data = car_filter(data)
+
+% Transpose data if it has more rows than columns
+if ndims(data) > 2 && size(data, 1) < size(data, 3)
+    data = permute(data, [3, 2, 1]);
+elseif size(data, 1) < size(data, 2)
+    data = data';
 end
-x_car = input;
+
+% Calculate the average potential across all electrodes for each trial
+average_potential = mean(data, 2, 'omitnan'); % Vectorized Common Average Referencing (CAR)
+
+% Subtract the average potential from each electrode's potential
+data = data - average_potential;
 end
