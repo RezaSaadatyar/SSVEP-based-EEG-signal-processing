092bf3cdb8ffaf05ca61eec4b43de50a0a47262f
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-03-29 14:18:02 +0330

    app

--- Code/MATLAB/app5_filter_car.m
+++ Code/MATLAB/app5_filter_car.m
@@ -12,7 +12,7 @@ data = load(filename); % Load the data from the selected mat file
 % Extract EEG data for the first trial (number samples, number channel, number trials)
 x = data.data1;  
 %% ------------------ Step 2: Filtering all trials --------------------------------
-fs = 256;
+fs = 256;           % Define sampling frequency
 f_low = 0.05;
 f_high = 100;
 order = 4;  
@@ -23,7 +23,7 @@ notch_filter = 'on';
 type_filter = "bandpass";   % low, high, bandpass
 filtered_data = filtering(x, f_low, f_high, order, fs, notch_freq, ...
      filter_active, notch_filter, quality_factor, type_filter);
-x_car = car_filter(filtered_data);
+x_car = car_filter(filtered_data);  % CAR filter
 %% --------------------------- Step 3: Plot Results ----------------------------
 time = (0: size(x, 1)-1)*1/fs;   % Time vector
 figure()
