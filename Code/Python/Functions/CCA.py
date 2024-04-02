66928eda0bd4fa5a3e005b8aa9e3c4e43ee46afe
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-04-02 19:25:01 +0330

    cca

---
+++ Code/Python/Functions/CCA.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+# ==================================== Canonical Correlation Analysis (CCA) ========================================
+def cca(data, fs, f_stim, num_channel, num_harmonic):
+    
+    """
+    ================================================================================================================
+    """
+    # ----------------------------- Convert data to ndarray if it's not already ------------------------------------
+    data = np.array(data) if not isinstance(data, np.ndarray) else data
+
+    # Transpose the data if it has more than one dimension and has fewer rows than columns
+    data = data.T if data.ndim > 1 and data.shape[0] < data.shape[-1] else data
+    # ------------------------------------------ Reference signal --------------------------------------------------
+    data_ref = []
+    time = np.arange(0, data.shape[0]) / fs  # Time vector
+
+    for _, val in enumerate(f_stim): # First loop: frequencies stimulation
+
+        signal_ref = []
+
+        for j in range(1, num_harmonic + 1):  # Generate a reference signal for each frequency stimulation
+            signal_ref.append(np.sin(2 * np.pi * j * val * time))
+            signal_ref.append(np.cos(2 * np.pi * j * val * time))
+
+        data_ref.append(np.stack(signal_ref, axis=1))  # Store data_ref in the data_ref_list
+    # ------------------------------------------ Correlation Analysis ----------------------------------------------
+    predict_label = np.zeros(data.shape[-1])  # Initialize label_predic array with zeros
+    
+    for i in range(data.shape[-1]): # Loop through all Trials
+        
+        s = np.zeros(len(f_stim))
+
+        for k in range(len(f_stim)):  # Second loop: Calculate CCA for frequencies stimulation
+            cano_corr = cca_analysis(data[:, num_channel, i], data_ref[k])
+            s[k] = np.max(cano_corr)
+        
+        predict_label[i] = np.argmax(s) # Predict label for the current trial
+  
+    return predict_label
+
+
+
+def cca_analysis(data, data_ref):
+    """
+    Canonical Correlation Analysis (CCA)
+    :param data: numpy array of data
+    :param data_ref: numpy array of reference data
+    :return: numpy array of optimum canonical correlations
+    """
+    """
+    ================================================================================================================
+    """
+    # -------------------------------- Convert data to ndarray if it's not already ---------------------------------
+    data = np.array(data) if not isinstance(data, np.ndarray) else data
+    data_ref = np.array(data_ref) if not isinstance(data_ref, np.ndarray) else data_ref
+    
+    # Transpose the data if it has more than one dimension and has fewer rows than columns
+    data, data_ref = [x.T if x.ndim > 1 and x.shape[0] < x.shape[-1] else x for x in [data, data_ref]]
+   
+    # Combine the data and reference data based on their dimensionality
+    xy = np.concatenate((data, data_ref), axis=1) if data.shape[1] <= data_ref.shape[1] else np.concatenate\
+        ((data_ref, data), axis=1)
+    
+    # Calculate covariance matrices
+    covariance = np.cov(xy, rowvar=False)
+    n = min(data.shape[1], data_ref.shape[1])
+    cx = covariance[:n, :n]
+    cy = covariance[n:, n:]
+    cxy = covariance[:n, n:]
+    cyx = covariance[n:, :n]
+    
+    # Solve the optimization problem using eigenvalue decomposition
+    # Adding np.eye(*cy.shape) * eps ensures numerical stability
+    eps = np.finfo(float).eps
+    corr_coef = np.linalg.inv(cy + np.eye(*cy.shape) * eps) @ cyx @ np.linalg.inv(cx + np.eye(*cx.shape) * eps) @ cxy
+    
+    # Eigenvalue decomposition and sorting
+    eig_vals = np.linalg.eigvals(corr_coef)
+    
+    # Set any small negative eigenvalues to zero, assuming they are due to numerical error
+    eig_vals[eig_vals < 0] = 0.0000000000000000000001
+    d_coeff = np.sort(np.real(eig_vals))[::-1]  # Only real parts, sorted in descending order
+    # d_coeff = np.sqrt(np.sort(np.real(eig_vals))[::-1])  # Only real parts, sorted in descending order
+    
+    return d_coeff[:n]  # Return the canonical correlations
