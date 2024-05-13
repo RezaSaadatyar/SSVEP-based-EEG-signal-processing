b0ab6761a523c8b46f06e81eacfb98f092ea1ca9
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2024-05-13 10:07:13 +0330

    filter

--- Code/Python/Functions/Filtering.py
+++ Code/Python/Functions/Filtering.py
@@ -4,10 +4,23 @@ from scipy import signal
 # =============================================== Filtering ==================================================
 # Function to apply digital filtering to data
 def filtering(data, f_low, f_high, order, fs, notch_freq, quality_factor, filter_active="on", notch_filter="on",
-              type_filter='bandpass'):
+              type_filter='bandpass', design_method="IRR"):
     """
     =============================== Presented by: Reza Saadatyar (2023-2024) =================================
     ================================ E-mail: Reza.Saadatyar@outlook.com ======================================
+    Example:
+    fs = 256;                  % Sampling frequency
+    order = 3; 
+    f_low = 0.5;
+    f_high = 100;
+    notch_freq = 50;
+    notch_filter = 'on';
+    filter_active = 'on';
+    design_method = "IIR";      % IIR, FIR
+    type_filter = "bandpass";   % low, high, bandpass
+    filtered_data = filtering(data_total, f_low, f_high, order, fs, notch_freq, filter_active,  notch_filter, 
+                              type_filter, design_method)
+                              
     FILTERING applies digital filtering to data.
     Inputs:
     - data: Input data to be filtered.
@@ -16,6 +29,7 @@ def filtering(data, f_low, f_high, order, fs, notch_freq, quality_factor, filter
     - order: Filter order.
     - fs: Sampling frequency.
     - notch_freq: Frequency to be notched out.
+    - quality_factor: Quality factor for the notch filter.
     - filter_active: Activate filtering ('on' or 'off').
     - notch_filter: Activate notch filtering ('on' or 'off').
     - type_filter: Type of filter ('low', 'high', 'bandpass', 'stop').
@@ -23,25 +37,19 @@ def filtering(data, f_low, f_high, order, fs, notch_freq, quality_factor, filter
     - filtered_data: Filtered data.
     ================================== Flowchart for the filtering function ==================================
     Start
-     1. Normalize frequency values (f_low, f_high)
-     2. Check the dimensions of the input data:
-        a. If it's a 3D matrix and has more rows than columns:
-           - Transpose the data using permute to make channels as slices
-        b. If it has more columns than rows:
-           - Transpose the data to make channels as rows
-     3. Design Butterworth filter based on the specified type:
-        - Lowpass filter: Design Butterworth filter with 'low' option
-        - Highpass filter: Design Butterworth filter with 'high' option
-        - Bandpass filter: Design Butterworth filter with 'bandpass' option
-        - Bandstop filter: Design Butterworth filter with 'bandstop' option
-     4. Design a notch filter:
-        - Use iirnotch to design a notch filter based on notch frequency and quality factor
-     5. Notch filter:
-        - Apply notch filtering if notch_filter is 'on'
-     6. Apply the digital filter using filtfilt:
-        - Apply filtering if filter_active is 'on'
-     7. Output the filtered data (filtered_data)
-     End
+    1. Normalize the frequency values by dividing them by half the sampling frequency 
+    (Nyquist frequency).
+    2. Transpose the data if it has more rows than columns or if specified by the user.
+    3. Design the filter based on the specified parameters and filter type:
+        a. If the design method is IIR:
+        - Design Butterworth filter using the 'butter' function.
+        b. If the design method is FIR:
+        - Design FIR filter using the 'fir1' function. 
+    4. Design a notch filter if specified by the user using the 'butter' function.
+    5. Apply the notch filter to the data if specified by the user using 'filtfilt'.
+    6. Apply the designed filter to the data if active using 'filtfilt'.
+    7. Return the filtered data.
+    End
     ==========================================================================================================
     """
     # ---------------------------------------- Normalize frequency values ------------------------------------
@@ -55,14 +63,25 @@ def filtering(data, f_low, f_high, order, fs, notch_freq, quality_factor, filter
     filtered_data = filtered_data.T if filtered_data.ndim > 1 and filtered_data.shape[0] > filtered_data.shape[-1]\
     else filtered_data
     # ------------------------- Design Butterworth filter based on the specified type ------------------------
-    if type_filter == "low":     
-        b, a = signal.butter(order, f_low, btype='low')
-    elif type_filter == "high":
-        b, a = signal.butter(order, f_high, btype='high')
-    elif type_filter == "bandpass":
-        b, a = signal.butter(order, [f_low, f_high], btype='bandpass')
-    elif type_filter == "bandstop":
-        b, a = signal.butter(order, [f_low, f_high], btype='bandstop')
+    if design_method == "IIR":
+        if type_filter == "low":     
+            b, a = signal.butter(order, f_low, btype='low')
+        elif type_filter == "high":
+            b, a = signal.butter(order, f_high, btype='high')
+        elif type_filter == "bandpass":
+            b, a = signal.butter(order, [f_low, f_high], btype='bandpass')
+        elif type_filter == "bandstop":
+            b, a = signal.butter(order, [f_low, f_high], btype='bandstop')
+    else:
+        a = 1
+        if type_filter == "low":     
+            b = signal.firwin(order, f_low, pass_zero='lowpass')
+        elif type_filter == "high":
+            b = signal.firwin(order, f_high, pass_zero='highpass')
+        elif type_filter == "bandpass":
+            b = signal.firwin(order, [f_low, f_high], pass_zero='bandpass')
+        elif type_filter == "bandstop":
+            b = signal.firwin(order, [f_low, f_high], pass_zero='bandstop')
     
     # Design a notch filter using signal.iirnotch
     b_notch, a_notch = signal.iirnotch(notch_freq, quality_factor, fs)
